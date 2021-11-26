d7efd0aca55752732ad5069923096e0f01e4cc4c
Luis Santamaria <luisignaciosantamariap@gmail.com>
2021-11-26 17:06:47 -0500

    Clase = Calculando el top 10% de salarios - Creamos un codigo para calcular el top 10 de salarios con datos ficticios - Creamos salarios.{html,js} y analisis.js - Clase 22

---
+++ salarios.js
@@ -0,0 +1,77 @@
+const colombia = [];
+colombia.push({
+    name: "Juanita",
+    salary: 1000
+});
+colombia.push({
+    name: "Ricardo",
+    salary: 1500
+});
+colombia.push({
+    name: "Nicolas",
+    salary: 1700
+});
+colombia.push({
+    name: "Sebastian",
+    salary: 1200
+});
+colombia.push({
+    name: "Manuel",
+    salary: 1300
+});
+colombia.push({
+    name: "Israel",
+    salary: 1600
+});
+colombia.push({
+    name: "Luis",
+    salary: 10000
+});
+colombia.push({
+    name: "Angel",
+    salary: 9900
+});
+colombia.push({
+    name: "Andres",
+    salary: 700
+});
+colombia.push({
+    name: "Patrick",
+    salary: 600
+});
+colombia.push({
+    name: "Diego",
+    salary: 900
+});
+colombia.push({
+    name: "Armin",
+    salary: 16000
+});
+colombia.push({
+    name: "Tails",
+    salary: 10000
+});
+colombia.push({
+    name: "Santiago",
+    salary: 1500
+});
+colombia.push({
+    name: "Gabriel",
+    salary: 2000
+});
+colombia.push({
+    name: "Elian",
+    salary: 400
+});
+colombia.push({
+    name: "William",
+    salary: 3000
+});
+colombia.push({
+    name: "Javier",
+    salary: 800
+});
+colombia.push({
+    name: "Mathias",
+    salary: 8000
+});
